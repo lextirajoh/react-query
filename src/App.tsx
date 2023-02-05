e3478f6d3f7b628a19d7e320e847cadb2018aeef
Lex <alext@riseup.net>
2023-02-05 23:27:14 +0100

    Code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { Contact } from './pages/Contact';
-import { Navbar } from './Navbar';
+
 
 export default function App() {
   const client = new QueryClient({});
@@ -13,12 +10,8 @@ export default function App() {
     <div className="App">
       <QueryClientProvider client={client}>
         <Router>
-          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
           </Routes>
         </Router>
       </QueryClientProvider>
